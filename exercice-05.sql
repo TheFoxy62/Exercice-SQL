515ca8ffebfdc6c85c41d4cc1a67c178cc2ce6ed
thefoxy62 <hugosoual.hs@gmail.com>
2022-06-30 18:16:38 +0200

    sql

--- exercice-05.sql
+++ exercice-05.sql
@@ -1,25 +1,38 @@
 -- Exo 5
 -- Requêtes avec jointure « many to many »
-SELECT *
-FROM 
-WHERE 
+
+
 -- Exo 5.1
 -- Listez tous les students avec leurs tags
-SELECT *
-FROM 
-WHERE 
+SELECT * 
+FROM student_tag 
+INNER JOIN student 
+ON student_tag.student_id = student.id 
+INNER JOIN tag 
+ON student_tag.tag_id = tag_id;
 -- Exo 5.2
 -- Listez tous les tags avec leurs students
-SELECT *
-FROM 
-WHERE 
+SELECT * 
+FROM `tag`
+INNER JOIN student_tag
+ON student_tag.tag_id = tag_id
+INNER JOIN student
+ON student_tag.student_id = student.id;
 -- Exo 5.3
 -- Listez le student dont l'id est `2` avec ses tags
-SELECT *
-FROM 
-WHERE 
+SELECT * 
+FROM `student`
+INNER JOIN student_tag
+ON student_tag.tag_id = tag_id
+INNER JOIN tag
+ON 	student_tag.student_id = student.id
+WHERE student_id = 2;
 -- Exo 5.4
 -- Listez le tag dont l'id est `2` avec ses students
-SELECT *
-FROM 
-WHERE 
+SELECT * 
+FROM `tag`
+INNER JOIN student_tag
+ON student_tag.student_id = tag_id
+INNER JOIN student
+ON student_tag.student_id = student_id
+WHERE tag_id =2;
